e0bab628fda2f100d610542612eef79a6322e5d0
Akila Lakshan weerathunga <119279370+Akilalakshanweerathunga@users.noreply.github.com>
2024-06-27 11:27:18 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyADnxCaeLTL5UZfNp2tuUjJ5gaejTjQj6k",
-        authDomain: "ashop-b4fd6.firebaseapp.com",
-        projectId: "ashop-b4fd6",
-        storageBucket: "ashop-b4fd6.appspot.com",
-        messagingSenderId: "724436502225",
-        appId: "1:724436502225:web:91904d56d4034e2f03a7f0",
-        measurementId: "G-HXWCL3JV7T"
+        apiKey: "#",
+        authDomain: "#",
+        projectId: "#",
+        storageBucket: "#",
+        messagingSenderId: "#",
+        appId: "#",
+        measurementId: "#T"
     }
   };
   
